8572a468e7e4f987eb798976c41ce066df275cce
Frank Blue <1305679132@qq.com>
2018-10-17 15:40:21 +0800

    debug with index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	else {
 		$pages = 1;
 	}
-	$article_sql = "select * from new_article order by time desc ";
+	$article_sql = "select * from new_article order by id desc ";
 	$article_query = mysql_query($article_sql);
 	if($article_query&&mysql_num_rows($article_query)){
 		while($article_row = mysql_fetch_assoc($article_query)){
