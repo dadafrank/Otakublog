9467959c2aaabba2372a348399247237e6a5fe78
Frank Blue <1305679132@qq.com>
2018-10-22 10:51:10 +0800

    prevent sql

--- blog_mess.handle.php
+++ blog_mess.handle.php
@@ -1,8 +1,11 @@
 <?php
 	require_once('connect.php');
 	$name=htmlspecialchars($_POST[mess_author]);
+	$name=str_replace("'","\'",$name);
 	$email=htmlspecialchars($_POST[mess_email]);
+	$email=str_replace("'","\'",$email);
 	$message=htmlspecialchars($_POST[mess_content]);
+	$message=str_replace("'","\'",$message);
 	$time=date("Y-m-d h:i:sa");
 	if($name==""||$message==""){
 		echo "<script>alert('评论或者姓名不能为空');window.location.href='blog_mess.php'</script>";
